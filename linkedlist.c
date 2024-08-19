524985733a4860f4c77d185d817655c89a40150f
takeshijan <nodirjonmuxammadaliyev@gmail.com>
2024-08-19 21:34:45 +0500

    memory leaks fixed

--- linkedlist.c
+++ linkedlist.c
@@ -9,51 +9,98 @@ typedef struct Node{
 Node *head = NULL;
 
 void print(){
-    Node *link = head;
-    while (link !=NULL) {
-        printf("%d ",link->data);
-        link = link->next;
+    Node *p = head;
+    while (p!= NULL) {
+        printf("%d ",p->data);
+        p = p->next;
     }
     printf("\n");
 }
 
 void insert_begin(int data){
-    Node *link = (Node *)malloc(sizeof(Node));
-    link -> data = data;
-    link -> next = head;
-    head = link;
+    Node *l = (Node *)malloc(sizeof(Node));
+    l->data = data;
+    l->next = head;
+    head = l;
 }
 
 void insert_end(int data){
-    Node *link = (Node *)malloc(sizeof(Node));
-    link -> data = data;
-    link -> next = NULL;
+    Node *l = (Node *)malloc(sizeof(Node));
+    l->data = data;
     if(head == NULL){
-        head = link;
+        head = l;
     }
     else {
         Node *linkedlist = head;
-
         while (linkedlist->next != NULL) {
             linkedlist = linkedlist->next;
         }
-
-        linkedlist->next = link;
+        linkedlist->next = l;
     }
 }
 
-int main(){
+void insert_after(Node *list,int data){
+    if(list == NULL){
+        printf("not NULL");
+        exit(0);
+    }
+    Node *l = (Node *)malloc(sizeof(Node));
+    l->data = data;
+    l->next = list->next;
+    list->next = l;
+}
 
-    insert_end(20);
-    insert_begin(0);
-    insert_end(100);
+void delete_begin(){
+    if(head == NULL){
+        printf("List is empty");
+        return;
+    }
+    Node *current = head;
+    head = head->next;
+    free(current);
+}
+
+void delete_end(){
+    if(head==NULL){
+        printf("List is empty");
+        return;
+    }
+    if(head->next == NULL){
+        free(head);
+        head = NULL;
+        return;
+    }
+    Node *current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+    Node *lastnode = current->next;
+    current->next = NULL;
+    free(lastnode);
+}
+
+int main() {
+
+
+    insert_end(12);
+    insert_begin(99);
+    insert_begin(2010201); 
+    insert_begin(21021);
+
+    delete_end();
+    delete_begin();
+
     print();
 
-    while (head != NULL) {
-        Node *cur = head;
-        head = head->next;
-        free(cur);
-    }
 
+    Node *current = head;
+    Node *next_list;
+    while (current !=NULL) {
+        next_list = current->next;
+        free(current);
+        current = next_list;
+    }
+    head = NULL;
+    
     return 0;
 }
